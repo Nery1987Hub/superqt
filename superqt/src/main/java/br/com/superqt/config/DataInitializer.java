8a9dda8f0effa2a32d187d495a44e76e7a9d35d4
Players-Garmadon <guilherme.almeida81@fatec.sp.gov.br>
2026-06-12 04:27:29 -0300

    Criando DataInitializer.java para criar o CRUD de ADMIN

---
+++ superqt/src/main/java/br/com/superqt/config/DataInitializer.java
@@ -0,0 +1,30 @@
+package br.com.superqt.config;
+
+import br.com.superqt.model.Aluno;
+import br.com.superqt.model.Perfil;
+import br.com.superqt.repository.AlunoRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+public class DataInitializer{
+    @Bean
+    CommandLineRunner initDatabase(AlunoRepository repository) {
+        return args -> {
+            // Verifica se o admin com esse CPF já existe para não duplicar
+            if (repository.findByCpfaluno("00000000000").isEmpty()) {
+                Aluno admin = new Aluno();
+                admin.setNome("Admin do Sistema");
+                admin.setCPFaluno("00000000000");
+                admin.setSenha("admin123"); // Em produção, aqui usaria criptografia
+                admin.setIdade(99);
+                admin.setPerfil(Perfil.ADMIN); // Define explicitamente como ADMIN
+
+                repository.save(admin);
+                System.out.println("--> Usuário ADMINISTRADOR criado com sucesso! (CPF: 00000000000)");
+            }
+        };
+    }
+}
