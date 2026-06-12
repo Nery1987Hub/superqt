64bec589313ddad4658fd5a4fc0622fd449c6c72
Players-Garmadon <guilherme.almeida81@fatec.sp.gov.br>
2026-06-12 00:52:51 -0300

    Atualizando AlunoRepository.java com findByCpfaluno para form de login

--- superqt/src/main/java/br/com/superqt/repository/AlunoRepository.java
+++ superqt/src/main/java/br/com/superqt/repository/AlunoRepository.java
@@ -1,9 +1,13 @@
 package br.com.superqt.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.superqt.model.Aluno;
 
 @Repository
-public interface AlunoRepository extends JpaRepository<Aluno, Long>{}
+public interface AlunoRepository extends JpaRepository<Aluno, Long>{
+    Optional<Aluno> findByCpfaluno(String cpfaluno);
+}
