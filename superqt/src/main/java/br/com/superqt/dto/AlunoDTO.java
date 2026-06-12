d6c123fb2c29aa1bd047d113ea4f72e99b54f9f3
Players-Garmadon <guilherme.almeida81@fatec.sp.gov.br>
2026-06-12 00:56:06 -0300

    Adicionando Perfil na DTO AlunoDTO.java para form de login

--- superqt/src/main/java/br/com/superqt/dto/AlunoDTO.java
+++ superqt/src/main/java/br/com/superqt/dto/AlunoDTO.java
@@ -1,5 +1,7 @@
 package br.com.superqt.dto;
 
+import br.com.superqt.model.Perfil;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -7,5 +9,7 @@ import jakarta.validation.constraints.NotNull;
 public record AlunoDTO(
     @NotBlank String senha,
     @NotNull @Min(0) int idade,
+    @NotBlank String nome,
+    @Enumerated Perfil perfil,
     @NotBlank String cpfaluno
 ){}
