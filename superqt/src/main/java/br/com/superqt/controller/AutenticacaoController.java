1c5194500ac7b124f18b6be570d1e5c9326cb2d3
Players-Garmadon <guilherme.almeida81@fatec.sp.gov.br>
2026-06-12 00:56:45 -0300

    Criando o controller AutenticacaoControlle.java para autenticar login de usuário

---
+++ superqt/src/main/java/br/com/superqt/controller/AutenticacaoController.java
@@ -0,0 +1,34 @@
+package br.com.superqt.controller;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.superqt.dto.LoginRequestDTO;
+import br.com.superqt.model.Aluno;
+import br.com.superqt.repository.AutenticacaoService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping("/auth")
+public class AutenticacaoController{
+    private final AutenticacaoService autenticacaoService;
+
+    AutenticacaoController(AutenticacaoService autenticacaoService) {
+        this.autenticacaoService = autenticacaoService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequestDTO dto){
+        try{
+            Aluno alunoAutenticado = autenticacaoService.autenticar(dto);
+
+            return ResponseEntity.ok(alunoAutenticado);
+        }catch (RuntimeException e){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
+    }
+}
